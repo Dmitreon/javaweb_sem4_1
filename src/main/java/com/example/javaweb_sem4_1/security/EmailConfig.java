182b63049930114a122b35b60d45e026278577e1
Techun <n.gashkin@gmail.com>
2024-06-22 20:12:24 +0300

    fix bug

--- src/main/java/com/example/javaweb_sem4_1/security/EmailConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.javaweb_sem4_1.security;
-
-public class EmailConfig {
-    public static final String USERNAME = "014999c6718847";
-    public static final String PASSWORD = "3b6e1a06cc99f8";
-}
